26a5e3e0a58e5ab6a377ed856f66953f132f5e30
Dee <deanna.iannantuono@gmail.com>
2023-10-12 11:43:11 +1000

    Added logo to navbar and added hamburger menu for tablet and mobile sizes. Navbar now complete

--- her_tech_collective/src/components/NavBar.jsx
+++ her_tech_collective/src/components/NavBar.jsx
@@ -1,10 +1,32 @@
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import "./NavBar.css";
 
 function NavBar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <div id="navlinks">
-            <nav>
+            <img src= "/logo.png" alt="Logo" className="logo" />
+            <div className="menu-toggle" onClick={toggleMenu}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className={`hamburger-icon ${menuOpen ? "open" : ""}`}
+                >
+                <path
+                    fillRule="evenodd"
+                    d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+                    clipRule="evenodd"
+                />
+            </svg>
+        </div>
+        <nav className={`nav-menu ${menuOpen ? "open" : ""}`}>
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/profiles/">The Collective</Link>
