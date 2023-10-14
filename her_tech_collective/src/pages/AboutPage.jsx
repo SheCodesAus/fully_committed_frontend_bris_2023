870820c3e578e2ead096eb391247e9c1a45c323f
jess-cameron <133481697+jess-cameron@users.noreply.github.com>
2023-10-14 12:59:00 +1000

    started about page

--- her_tech_collective/src/pages/AboutPage.jsx
+++ her_tech_collective/src/pages/AboutPage.jsx
@@ -1,5 +1,30 @@
+import React from "react";
+
 function AboutPage() {
-    return <h1>This is the about page</h1>
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>
+            Her Tech Collective envisions a world where young women in our community are empowered 
+            to pursue and excel in the tech industry. Our vision is to light the path of inspiration 
+            by presenting a diverse array of relatable role models who thrive in the world of 
+            technology. We are committed to proving that, indeed, anyone can work in tech.
+            
+            Her Tech Collective is committed to simplifying the path to involvement. Our vision is 
+            to make it effortless for individuals to log in, create their profiles, explore the rich 
+            diversity of tech role models, and connect with those they aspire to learn from. 
+            We believe that every click, every connection, and every moment spent on our platform 
+            is a step toward a more diverse and innovative tech community.
+            </p>
+            <h2>Get the most out of this site</h2>
+            <p>Inspiration</p>
+            <p>Recruitment</p>
+            <p>Mentoring</p>
+            <p>Development</p>          
+          </div>
+
+    );   
+
 }
 
 export default AboutPage;
