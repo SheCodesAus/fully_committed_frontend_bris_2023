4795824cb2012d258215a3a0725c56ef11fc0125
eoryazantseva <eoryazantseva@gmail.com>
2023-10-16 12:22:50 +1000

    added link to the profile on the card

--- her_tech_collective/src/components/ProfileCard/ProfileCard.jsx
+++ her_tech_collective/src/components/ProfileCard/ProfileCard.jsx
@@ -1,23 +1,18 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "./ProfileCard.css";
-
-// import useProfile from "../../hooks/use-profile";
 
 function ProfileCard(props) {
     const { profileData } = props;
 
-    // const profileLink = `profiles/${profileData.id}/`;
+    const profileLink = `${profileData.id}/`;
 
     console.log('profile data in profileCARD: ', profileData)
-// const {profile, isLoading, error} = useProfile(profileData.id)
-// const skills = profile?.skills || []
-// const location = profile?.location || []
 
 
 return (
     <div className="profile-card">
-        {/* <Link to={profileLink}> */}
+        <Link to={profileLink}>
         <img src={profileData.image_url} />
         <h3>{profileData.profile_name}</h3>
         <h4>{profileData.location.location_name}</h4>
@@ -26,6 +21,7 @@ return (
                     <li key={skill.id}>{skill.skill_name}</li>
                 ))}
         </ul>
+        </Link>
     </div>
 );
 }
