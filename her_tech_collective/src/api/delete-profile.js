277250853356169acab0b2585b0e3b7ed28cce3b
eoryazantseva <eoryazantseva@gmail.com>
2023-10-21 11:55:37 +1000

    delete-update functionality

---
+++ her_tech_collective/src/api/delete-profile.js
@@ -0,0 +1,31 @@
+async function deleteProfile(profile_id) {
+    
+    const url =`https://hertechcollective.fly.dev/projects/${profile_id}`;
+    const response = await fetch(url, { 
+            method:"DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Token ${window.localStorage.getItem("token")}`,
+            },
+        });
+
+    // Check if the request was successful
+
+        if (!response.ok) {
+            const fallbackError =`Error deleting project with id ${profile_id}`;
+            const data = await response.json().catch(() => {
+                throw new Error(fallbackError);
+        });
+
+    
+            const errorMessage = data?.detail ?? fallbackError;
+            throw new Error(errorMessage);
+        }
+
+        // Return a success message or any other relevant data
+
+        return "Profile deleted successfully";
+
+}
+
+export default deleteProfile;
--- her_tech_collective/src/pages/ProfilePage.jsx
+++ her_tech_collective/src/pages/ProfilePage.jsx
@@ -1,14 +1,20 @@
 import { useParams } from "react-router-dom";
 import useProfile from "../hooks/use-profile";
 import "./ProfilePage.css";
+import deleteProfile from "../api/delete-profile";
 
 function ProfilePage() {
 
     // Here we use a hook that comes for free in react router called `useParams`to get the id from the URL so that we can pass it to our useProfile hook.
     const { id } = useParams();
+    
 
      // useProfile returns three pieces of info, so we need to grab them all here
     const {profile, isLoading, error} = useProfile(id)
+
+    // Setting up delete confirmation
+
+    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
 
     // console.log("I am still looking for the profile: ", isLoading)
     console.log("Testing profile: ", profile)
@@ -21,6 +27,54 @@ function ProfilePage() {
     if (error) {
         return (<p>{error.message}</p>);
         }
+
+
+
+    // Handle project deletion
+
+    const handleDelete = () => {
+        // Show the delete confirmation
+        setShowDeleteConfirmation(true);
+    };
+
+    const handleConfirmDelete = () => {
+        // delete profile
+            deleteProfile(id)
+            .then(() => {
+                //Redirect to the homepage
+                navigate("/");
+            })
+            .catch ((error) => {
+                console.log("Error deleting profile: ", error);
+                // Handle error, e.g., display an error message to the user
+            });
+        };
+
+    const handleCancelDelete = () => {
+        // Hide the delete confirmation
+        setShowDeleteConfirmation(false);
+        };
+
+
+    const renderUpdateAndDeleteButtons = () => {
+        // Check if the authenticated user is the owner of the project
+        if (parseInt(auth.userId) == parseInt(profile.owner)) {
+          // Authenticated user is the owner, render the buttons
+            return (
+                <>
+                    <button className="delete-button" onClick={handleDelete}>
+                    Delete Profile
+                    </button>
+                    <button className="update-button">
+                        <Link to={`/update-profile/${profile.id}`}>Update Profile</Link>
+                    </button>
+                </>
+            );
+        }
+        // Authenticated user is not the owner, do not render the buttons
+        return null;
+        };
+
 
     return (
     <main>
@@ -38,6 +92,14 @@ function ProfilePage() {
                     ))}
                 </ul>
             </h4>
+            {showDeleteConfirmation && (
+                    <div className="delete-confirmation">
+                        <p>Are you sure you want to delete this profile?</p>
+                        <button onClick={handleConfirmDelete}>Yes</button>
+                        <button onClick={handleCancelDelete}>No</button>
+                    </div>
+                )}
+                {renderUpdateAndDeleteButtons()}
         </div>
     </main>
     );
