7755d74d83c235b075b0da3308516d31e9f54b0a
eoryazantseva <eoryazantseva@gmail.com>
2023-10-26 23:25:25 +1000

    added success message and navigating to the collective page on the successful profile creation

--- her_tech_collective/src/components/Form/CreateProfileForm.jsx
+++ her_tech_collective/src/components/Form/CreateProfileForm.jsx
@@ -1,8 +1,10 @@
 import { useState,useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import getSkills from "../../api/get-skills";
 import getLocations from "../../api/get-locations";
 import postProfile from "../../api/post-profiles";
-import "./CreateProfileForm.css"
+import "./CreateProfileForm.css";
+
 
 
 function CreateProfileForm() {
@@ -22,27 +24,29 @@ function CreateProfileForm() {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [query, setQuery] = useState([]);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
+  const navigate = useNavigate();
   
-  
- useEffect(() => {
-   getSkills()
-     .then((skillsData) => {
-       setSkills(skillsData);
-       setIsLoading(false);
-     })
-     .catch((error) => {
-       setError(error);
-       setIsLoading(false);
-     });
-   getLocations()
-     .then((locationsData) => {
-       setLocations(locationsData);
-     })
-     .catch((error) => {
-       setError(error);
-     });
- }, []);
+  useEffect(() => {
+    getSkills()
+      .then((skillsData) => {
+        setSkills(skillsData);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+    getLocations()
+      .then((locationsData) => {
+        setLocations(locationsData);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  }, []);
 // const [skillsIsLoading, setSkillsIsLoading] = useState(true);
 // const [locationsIsLoading, setLocationsIsLoading] = useState(true);
 
@@ -152,123 +156,136 @@ const handleQueryChange = (event) => {
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    setIsLoading(true)
+
     postProfile(profileData)
       .then(() => {
-        navigate("/");
+        setSuccessMessage("The profile has been created successfully");
+        setFormSubmitted(true);
+
+        setTimeout(() => {
+          navigate("/profiles/");
+        }, 2500);
       })
       .catch(() => {
-        setIsLoading(false);
+
+        setError("Error creating the profile")
       });
   };
 
 
     return (
-      <div className="form-container">
-        <form onSubmit={handleSubmit}>
-          <h1>Create Her Tech Collective Profile</h1>
-          <div>
-              <label htmlFor="profile_name"></label>
+      <>
+        {formSubmitted ? (
+            <div className="success-message">{successMessage}</div>
+        ) : (
+        <div className="form-container">
+            <form onSubmit={handleSubmit}>
+              <h1>Create Her Tech Collective Profile</h1>
+              <div>
+                  <label htmlFor="profile_name"></label>
+                  <input
+                id="profile_name"
+                type="text"
+                placeholder="Profile name"
+                onChange={handleChange}/>
+            </div>
+            <div>
+              <label htmlFor="job_title"></label>
               <input
-            id="profile_name"
-            type="text"
-            placeholder="Profile name"
-            onChange={handleChange}/>
-        </div>
-        <div>
-          <label htmlFor="job_title"></label>
-          <input
-            id="job_title"
-            type="text"
-            placeholder="Job Title"
-            onChange={handleChange}/>
-        </div>
-        <div>
-          <label htmlFor="linkedin_url"></label>
-          <input
-            id="linkedin_url"
-            type="url"
-            placeholder="LinkedIn URL"
-            onChange={handleChange}/>
-        </div>
-        <div>
-          <label htmlFor="image_url"></label>
-          <input
-            id="image_url"
-            type="url"
-            placeholder="Image URL"
-            onChange={handleChange}/>
-        </div>
-        <div>
-          <label htmlFor="bio"></label>
-          <input
-            id="bio"
-            type="textarea"
-            placeholder="Bio"
-            onChange={handleChange}/>
-        </div>
-        <div className="isactive">
-          <label htmlFor="is_active">Is_active</label>
-          <input
-            id="is_active"
-            type="checkbox"
-            onChange={handleChecked}/>
-        </div>
-        <div>
-          <label htmlFor="location" style={{ marginRight: '20px' }}>Location</label>
-          {/* <select id="location">
-            <option value="Brisbane">Brisbane</option>
-            <option value="Sydney">Sydney</option>
-          </select> */}
-          {/* <ul>
-            {locations.map((item) => (
-              <li key={item.id}>{item.location_name}</li>
-            ))}
-          </ul> */}
-          <select id="location" onChange={handleSelected}>
-            <option value="" disabled selected>Select a location</option>
-            {locations.map((item) => (
-              <option
-                key={item.id}
-                // value={item.id}
-                value={JSON.stringify({
-                  id: item.id,
-                  location_name: item.location_name,
-                })}
-              >
-                {item.location_name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label htmlFor="skills">Skills</label>
-          <div id="skills">
-            {skills.map((item) => (
-              <label key={item.id}>
-                <input
-                  type="checkbox"
-                  id={item.id}
-                  name={item.skill_name}
-                  value={JSON.stringify(item)}
-                  onChange={handleQueryChange}
-                />
-                {item.skill_name}
-              </label>
-            ))}
-          </div>
+                id="job_title"
+                type="text"
+                placeholder="Job Title"
+                onChange={handleChange}/>
+            </div>
+            <div>
+              <label htmlFor="linkedin_url"></label>
+              <input
+                id="linkedin_url"
+                type="url"
+                placeholder="LinkedIn URL"
+                onChange={handleChange}/>
+            </div>
+            <div>
+              <label htmlFor="image_url"></label>
+              <input
+                id="image_url"
+                type="url"
+                placeholder="Image URL"
+                onChange={handleChange}/>
+            </div>
+            <div>
+              <label htmlFor="bio"></label>
+              <input
+                id="bio"
+                type="textarea"
+                placeholder="Bio"
+                onChange={handleChange}/>
+            </div>
+            <div className="isactive">
+              <label htmlFor="is_active">Is_active</label>
+              <input
+                id="is_active"
+                type="checkbox"
+                onChange={handleChecked}/>
+            </div>
+            <div>
+              <label htmlFor="location" style={{ marginRight: '20px' }}>Location</label>
+              {/* <select id="location">
+                <option value="Brisbane">Brisbane</option>
+                <option value="Sydney">Sydney</option>
+              </select> */}
+              {/* <ul>
+                {locations.map((item) => (
+                  <li key={item.id}>{item.location_name}</li>
+                ))}
+              </ul> */}
+              <select id="location" onChange={handleSelected}>
+                <option value="" disabled selected>Select a location</option>
+                {locations.map((item) => (
+                  <option
+                    key={item.id}
+                    // value={item.id}
+                    value={JSON.stringify({
+                      id: item.id,
+                      location_name: item.location_name,
+                    })}
+                  >
+                    {item.location_name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label htmlFor="skills">Skills</label>
+              <div id="skills">
+                {skills.map((item) => (
+                  <label key={item.id}>
+                    <input
+                      type="checkbox"
+                      id={item.id}
+                      name={item.skill_name}
+                      value={JSON.stringify(item)}
+                      onChange={handleQueryChange}
+                    />
+                    {item.skill_name}
+                  </label>
+                ))}
+              </div>
 
-          {/* <select id="skills" multiple required onChange={handleSelected}>
-            {skills.map((item) => (
-              <option key={item.id} value={JSON.stringify(item)}>
-                {item.skill_name}
-              </option>
-            ))}
-          </select> */}
+              {/* <select id="skills" multiple required onChange={handleSelected}>
+                {skills.map((item) => (
+                  <option key={item.id} value={JSON.stringify(item)}>
+                    {item.skill_name}
+                  </option>
+                ))}
+              </select> */}
+            </div>
+            <input type="submit" value="Submit" className="submit-button"></input>
+          </form>
+          {error && <p>Error: {error}</p>}
         </div>
-        <input type="submit" value="Submit" className="submit-button"></input>
-      </form>
-      </div>
+        )}
+      </>
     );
 }
 export default CreateProfileForm;
